e8f4f95d8a45c0a20b5f8462401c107d3333bf0c
Tarun1026 <tarun08531234@gmail.com>
2024-06-23 02:38:05 +0530

    Use Dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NowPlayingMovies from './NowPlaying/NowPlayingMovies';
-import MovieApi from './PopularMovieAPI/MovieApi';
-import UpComingMovies from './UpComingMoviesAPI/UpComingMovies';
 import HomePage from './Home/HomePage';
-import TopRatedMovies from './TopRated/TopRatedMovies';
+
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/popular" element={<MovieApi />} />
-        <Route exact path="/upcoming" element={<UpComingMovies />} />
-        <Route exact path="/top-rated" element={<TopRatedMovies />} />
-        <Route exact path="/now-playing" element={<NowPlayingMovies />} />
-      </Routes>
-    </Router>
+<HomePage/>
   );
 }
 
--- src/Home/HomePage.jsx
+++ src/Home/HomePage.jsx
@@ -1,34 +1,52 @@
-import React from 'react';
+import React, { useState } from "react";
 import { BiSolidMoviePlay } from "react-icons/bi";
-import './HomePage.css';
+import "./HomePage.css";
 import { Link } from "react-router-dom";
+import MovieApi from "../PopularMovieAPI/MovieApi";
+import UpComingMovies from "../UpComingMoviesAPI/UpComingMovies";
+import NowPlayingMovies from "../NowPlaying/NowPlayingMovies";
+import TopRatedMovies from "../TopRated/TopRatedMovies";
+
+// import
 
 function HomePage() {
+  const [selected, setSelected] = useState("");
+  const [isBackgroundVisible, setIsBackgroundVisible] = useState(true);
+
+  const handleChange = (e) => {
+    setSelected(e.target.value);
+    setIsBackgroundVisible(false);
+  };
+
   return (
     <>
-        <div className="home-page-background">
-      <div className='div-1'>
-        MOVIES 
-        
-        
-      </div>
-      <div className='div-2'>
-        <div className='div-3'>
-          <Link to="/popular"  className='links'>Popular Movies</Link>
-        </div> 
-        <div className='div-3'>
-          <Link to="/upcoming" className='links'>Upcoming Movies</Link>
+      <div
+        className={`home-page-background ${
+          !isBackgroundVisible ? "hidden" : ""
+        }`}
+      >
+        <div className="div-1">MOVIES</div>
+        <div className="div-2">
+          <select
+            value={selected}
+            onChange={(e) => handleChange(e)}
+            className="dropdown"
+          >
+            <option value="" disabled>
+              Select Movies
+            </option>
+            <option>Popular Movies</option>
+            <option>Upcoming Movies</option>
+            <option>Top Rated Movies</option>
+            <option>Now Playing Movies</option>
+          </select>
+
+          {selected == "Popular Movies" ? <MovieApi /> : ""}
+          {selected == "Upcoming Movies" ? <UpComingMovies /> : ""}
+          {selected == "Top Rated Movies" ? <TopRatedMovies /> : ""}
+          {selected == "Now Playing Movies" ? <NowPlayingMovies /> : ""}
+         
         </div>
-        <div className='div-3'>
-          <Link to="/top-rated" className='links'>Top Rated Movies</Link>
-        </div>
-        <div className='div-3'>
-          <Link to="/now-playing" className='links'>Now Playing Movies</Link>
-        </div>
-   
-      
-       
-      </div>
       </div>
     </>
   );

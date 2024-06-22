d75c98253cf8038593d3ae3aa74ead379d1eb62e
Tarun1026 <tarun08531234@gmail.com>
2024-06-22 07:03:27 +0530

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NowPlayingMovies from './NowPlaying/NowPlayingMovies';
 import MovieApi from './PopularMovieAPI/MovieApi';
 import UpComingMovies from './UpComingMoviesAPI/UpComingMovies';
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import ProtectedRoute from './ProtectedRoute'
 import HomePage from './Home/HomePage';
-import './App.css';
 import TopRatedMovies from './TopRated/TopRatedMovies';
 
 function App() {
-
   return (
-    <>
     <Router>
       <Routes>
-
-<Route exact path='/' element={<HomePage/>}/>
-<Route exact path='/popular' element={<MovieApi/>}/>
-<Route exact path='/upcoming' element={<UpComingMovies/>}/>
-<Route exact path='/top-rated' element={ <TopRatedMovies />}/>
-<Route exact path='/now-playing' element={<NowPlayingMovies/>}/>
-
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/popular" element={<MovieApi />} />
+        <Route exact path="/upcoming" element={<UpComingMovies />} />
+        <Route exact path="/top-rated" element={<TopRatedMovies />} />
+        <Route exact path="/now-playing" element={<NowPlayingMovies />} />
       </Routes>
-
-  </Router>
-  </>
+    </Router>
   );
 }
 
